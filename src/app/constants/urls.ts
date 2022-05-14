b5882f40fb103aa9fcac4e33a9926841fea47bf3
doc7720 <bodyazastavnyi97@gmail.com>
2022-05-14 16:53:25 +0300

    add hw 1

--- src/app/constants/urls.ts
+++ src/app/constants/urls.ts
@@ -2,10 +2,11 @@ import {environment} from "../../environments/environment";
 
 const {API: baseURL} = environment;
 
-export const urls
+export const urls =
 {
-  users: `${baseURL}/users`
-  posts: `${baseURL}/posts`
-  comments: `${baseURL}/comments`
+  users: `${baseURL}/users`,
+  posts: `${baseURL}/posts`,
+  comments: `${baseURL}/comments`,
 }
+
 
